bfb9e4b90513602faea2e8971b0491a259492947
heitsiorinirina-lgtm <heitsiorinirina@gmail.com>
2026-03-22 14:49:09 +0300

    fix: isolated pagination-template for more readability

--- templates/blog-templates.js
+++ templates/blog-templates.js
@@ -75,41 +75,6 @@ function youtubeTemplate(videos) {
     </div>
   `;
 }
-function createPaginationTemplate(current, total) {
-  if (total <= 1) return "";
-
-  const pages = Array.from({ length: total }, (_v, i) => i + 1);
-
-  return `
-    <nav class="pagination is-large-desktop is-centered" role="navigation" aria-label="pagination">
-      <ul class="pagination-list">
-        <li>
-          <a class="pagination-link" data-page="${current - 1}" ${
-            current === 1 ? "disabled" : ""
-          }>← Prev</a>
-        </li>
-        ${pages
-          .map(
-            (page) => `
-              <li>
-                <a class="pagination-link ${
-                  page === current ? "is-current" : ""
-                }" data-page="${page}">
-                  ${page}
-                </a>
-              </li>
-            `,
-          )
-          .join("")}
-        <li>
-          <a class="pagination-link" data-page="${current + 1}" ${
-            current === total ? "disabled" : ""
-          }>Next →</a>
-        </li>
-      </ul>
-    </nav>
-  `;
-}
 
 function tagsTemplate(tags) {
   return tags
